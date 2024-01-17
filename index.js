252cd2e22c3867a704f4c16fed5242fd4d01b7d2
marcuzzi1 <quentin.marcuzzi@etu.unilim.fr>
2024-01-17 10:47:14 +0100

    Uninstalled watcher as it doesn't fit with this project

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// Imports
-import Watcher from 'watcher'; // File watcher
+// Parsing args
+var parsedArgs = require('minimist')(process.argv.slice(2)); // Parse arguments with minimist
+var path = parsedArgs.path.toString() || undefined; // Extracting path to watch (from args)
 
-// Watching the path
+// Verifying if path is defined (don't ask me why, but it's not working without verifying 'true', must be from minimist...)
+// PS: If you have a better idea or a fix, please tell me by opening a PR or an issue
+if (path === undefined || path === 'true') {
+    console.error(`Path must be defined. Use command like that: npm start "path/to/watch"`); // Fancy error for the user
+    process.exit(1); // Exit process
+}
